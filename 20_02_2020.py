9e2054bc1fa1f528faf01e827510d5bc69b2d05c
user <jismonjoseph30@gmail.com>
2020-02-20 10:02:40 +0200

    20_02_2020

---
+++ 20_02_2020.py
@@ -0,0 +1,130 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(4)
+4
+>>> 
+>>> type(Hello, World!')
+	 
+SyntaxError: invalid syntax
+>>> 
+	 
+>>> 
+	 
+>>> type ('Hello, World!')
+	 
+<class 'str'>
+>>> type(17)
+	 
+<class 'int'>
+>>> 
+	 
+>>> type(3.2)
+	 
+<class 'float'>
+>>> 
+	 
+>>> type('17')
+	 
+<class 'str'>
+>>> type('3.2')
+	 
+<class 'str'>
+>>> 
+	 
+>>> print(1,000,000)
+	 
+1 0 0
+>>> 
+	 
+>>> type(1,001,002)
+	 
+SyntaxError: invalid token
+>>> print(1,001,002)
+	 
+SyntaxError: invalid token
+>>> 
+	 
+>>> message = 'And now for something completely different'
+	 
+>>> n =17
+	 
+>>> pi =3.141592653589793
+	 
+>>> print(pi)
+	 
+3.141592653589793
+>>> 
+	 
+>>> type(message)
+	 
+<class 'str'>
+>>> type(n)
+	 
+<class 'int'>
+>>> type(pi)
+	 
+<class 'float'>
+>>> 
+	 
+>>> 76trombones ='big parade'
+	 
+SyntaxError: invalid syntax
+>>> more@ = 1000000
+	 
+SyntaxError: invalid syntax
+>>> class ='Advanced Theoretical Zymurgy'
+	 
+SyntaxError: invalid syntax
+>>> 
+	 
+>>> print(1)
+	 
+1
+>>> x=2
+	 
+>>> print(x)
+	 
+2
+>>> 20+32
+	 
+52
+>>> hour-1
+	 
+Traceback (most recent call last):
+  File "<pyshell#35>", line 1, in <module>
+    hour-1
+NameError: name 'hour' is not defined
+>>> hour*60
+	 
+Traceback (most recent call last):
+  File "<pyshell#36>", line 1, in <module>
+    hour*60
+NameError: name 'hour' is not defined
+>>> 5**3
+	 
+125
+>>> minute =59
+	 
+>>> minute/60
+	 
+0.9833333333333333
+>>> minute//60
+	 
+0
+>>> 33/4
+	 
+8.25
+>>> 33//4
+	 
+8
+>>> 17
+	 
+17
+>>> x
+	 
+2
+>>> x+17
+	 
+19
+>>> 
